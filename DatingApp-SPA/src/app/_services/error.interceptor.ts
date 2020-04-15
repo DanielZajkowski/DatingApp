70b3cdbc93e007d0d7ba10908668e5a11fd93c27
DanielZajkowski <zajkowskidaniel@gmail.com>
2020-04-15 10:44:53 +0200

    commit

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -1,38 +1,42 @@
 import { Injectable } from '@angular/core';
 import {
   HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
   HttpErrorResponse,
   HTTP_INTERCEPTORS,
 } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   intercept(
-    req: import('@angular/common/http').HttpRequest<any>,
-    next: import('@angular/common/http').HttpHandler
-  ): import('rxjs').Observable<import('@angular/common/http').HttpEvent<any>> {
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error) => {
-        if (error.status === 400) {
-          return throwError(error.statusText);
-        }
         if (error instanceof HttpErrorResponse) {
+          if (error.status === 401) {
+            // console.log(error);
+            return throwError(error.statusText);
+          }
           const applicationError = error.headers.get('Application-Error');
           if (applicationError) {
             return throwError(applicationError);
           }
-          const severError = error.error;
-          let modelStateErrors = '';
-          if (severError.errors && typeof severError.errors === 'object') {
-            for (const key in severError.errors) {
-              if (severError.errors[key]) {
-                modelStateErrors += severError.errors[key] + '\n';
+          const serverError = error.error;
+          let modalStateErrors = '';
+          if (serverError.errors && typeof serverError.errors === 'object') {
+            for (const key in serverError.errors) {
+              if (serverError.errors[key]) {
+                modalStateErrors += serverError.errors[key] + '\n';
               }
             }
           }
-          return throwError(modelStateErrors || severError || 'Server Error');
+          return throwError(modalStateErrors || serverError || 'Server Error');
         }
       })
     );
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { NavComponent } from './nav/nav.component';
 import { from } from 'rxjs';
@@ -12,23 +13,14 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      FormsModule
-   ],
-   providers: [
-      AuthService,
-      ErrorInterceptorProvider
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+  ],
+  providers: [AuthService, ErrorInterceptorProvider],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
